6c44c8394971e2332a8c2190e84dae0a4ab4b34d
SamuelValentin <samuellv89@gmail.com>
2022-08-02 17:26:28 -0300

    informacoes da api na parte 1 e 2

--- app/Http/Controllers/GamesController.php
+++ app/Http/Controllers/GamesController.php
@@ -15,16 +15,18 @@ class GamesController extends Controller{
     public function index()
     {
 
-        $before = Carbon::now()->subMonth(6)->timestamp;
+        $before = Carbon::now()->subMonth(12)->timestamp;
+        $before1 = Carbon::now()->subMonth(6)->timestamp;
         $after = Carbon::now()->addMonth(2)->timestamp;
+        $current = Carbon::now()->timestamp;
 
         $popularGames = Http::withHeaders(config('services.igdb'))
         ->withBody("
-            fields name, cover.url, first_release_date, platforms.abbreviation, rating;
+            fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count;
             where platforms = (49)
             & (first_release_date > {$before}
             & first_release_date < {$after})
-            & rating > 75;
+            & rating_count > 20;
             sort rating desc;
             limit 10;
         
@@ -33,9 +35,25 @@ class GamesController extends Controller{
 
         dump($popularGames);
 
+        $recentlyReview = Http::withHeaders(config('services.igdb'))
+        ->withBody("
+            fields name, cover.url, first_release_date, platforms.abbreviation, rating, rating_count, summary;
+            where platforms = (49)
+            & (first_release_date > {$before1}
+            & first_release_date < {$current})
+            & rating > 75;
+            sort rating desc;
+            limit 3;
+        
+        ",'text/plain')
+        ->post('https://api.igdb.com/v4/games/')->json();
+
+        dump($recentlyReview);
+
         return view('index', [
-            'popularGames' => $popularGames]
-        );
+            'popularGames' => $popularGames,
+            'recentlyReview' => $recentlyReview,
+        ]);
     }
 
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -21,7 +21,9 @@
                             </div>
                         </div>
                     </div>
-                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">{{ $game['name'] }}</a>
+                    <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-8">
+                        {{ $game['name'] }}
+                    </a>
                     <div class="text-gray-400 mt-1">
                         {{-- @foreach ($game['platforms'] as $platform)
                             @if (array_key_exists('abbreviation', $platform))
@@ -38,75 +40,31 @@
         <div class="flex flex-col lg:flex-row my-10">
             <div class="recently-reviewed w-full lg:w-3/4 mr-0 lg:mr-32">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
-                <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg shadown-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img src="/alyx.jpeg" alt="game over" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:-20px; bottom: -20px">
-                                <div class="font-semibolb text-xs flex justify-center items-center h-full">98%</div> 
+                @foreach ($recentlyReview as $game)
+                    <div class="recently-reviewed-container space-y-12 mt-8">   
+                        <div class="game bg-gray-800 rounded-lg shadown-md flex px-6 py-6">
+                            <div class="relative flex-none">
+                                <a href="#">
+                                    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="game over" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
+                                </a>
+                                <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:-20px; bottom: -20px">
+                                    <div class="font-semibolb text-xs flex justify-center items-center h-full">
+                                        {{ round($game['rating']).'%' }}
+                                    </div> 
+                                </div>
+                            </div>
+                            <div class="ml-12">
+                                <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">{{ $game['name'] }}</a>
+                                <div class="text-gray-400 mt-1">{{ implode(', ', array_column($game['platforms'], 'abbreviation')) }}</div>
+                                <p class="mt-6 text-gray-400 hidden lg:block">
+                                    {{ $game['summary'] }}
+                                </p>
                             </div>
                         </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">Half-Life: Alyx</a>
-                            <div class="text-gray-400 mt-1">PC</div>
-                            <p class="mt-6 text-gray-400 hidden lg:block">
-                                Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.
-
-                                Você jogará como Alyx Vance, a única chance de sobrevivência da humanidade. O controle que a Combine exerce sobre o planeta desde o incidente em Black Mesa cresce cada vez mais, à medida que aprisionam o resto da população em cidades. Entre os sobreviventes, estão alguns dos grandes cientistas da Terra: você e o seu pai, o Dr. Eli Vance.
-                            </p>
-                        </div>
                     </div>
-
-                </div>
-
-                <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg shadown-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img src="/cyberpunk.jpeg" alt="game over" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:-20px; bottom: -20px">
-                                <div class="font-semibolb text-xs flex justify-center items-center h-full">70%</div> 
-                            </div>
-                        </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">Cyberpunk 2077</a>
-                            <div class="text-gray-400 mt-1">Xbox SX & PC</div>
-                            <p class="mt-6 text-gray-400 hidden lg:block">
-                                Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.
-
-                                Você jogará como Alyx Vance, a única chance de sobrevivência da humanidade. O controle que a Combine exerce sobre o planeta desde o incidente em Black Mesa cresce cada vez mais, à medida que aprisionam o resto da população em cidades. Entre os sobreviventes, estão alguns dos grandes cientistas da Terra: você e o seu pai, o Dr. Eli Vance.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
-
-                <div class="recently-reviewed-container space-y-12 mt-8">
-                    <div class="game bg-gray-800 rounded-lg shadown-md flex px-6 py-6">
-                        <div class="relative flex-none">
-                            <a href="#">
-                                <img src="/doom.jpeg" alt="game over" class="w-48 hover:opacity-75 transition ease-in-out duration-150">
-                            </a>
-                            <div class="absolute bottom-0 right-0 w-16 h-16 bg-gray-900 rounded-full" style="right:-20px; bottom: -20px">
-                                <div class="font-semibolb text-xs flex justify-center items-center h-full">90%</div> 
-                            </div>
-                        </div>
-                        <div class="ml-12">
-                            <a href="#" class="block text-base font-semibold leading-tight hover:text-gray-400 mt-4">DOOM</a>
-                            <div class="text-gray-400 mt-1">Xbox Series X</div>
-                            <p class="mt-6 text-gray-400 hidden lg:block">
-                                Half-Life: Alyx é a volta da Valve à série Half-Life em realidade virtual. Trata-se da história de uma impossível luta contra uma raça alienígena cruel, conhecida como Combine, situada entre os eventos de Half-Life e Half-Life 2.
-
-                                Você jogará como Alyx Vance, a única chance de sobrevivência da humanidade. O controle que a Combine exerce sobre o planeta desde o incidente em Black Mesa cresce cada vez mais, à medida que aprisionam o resto da população em cidades. Entre os sobreviventes, estão alguns dos grandes cientistas da Terra: você e o seu pai, o Dr. Eli Vance.
-                            </p>
-                        </div>
-                    </div>
-
-                </div>
+                @endforeach
             </div>
+
             <div class="most-anticipated lg:w-1/4 mt-12 lg:mt-0">
                 <h2 class="text-blue-500 uppercase tracking-wide font-semibold">Recently Reviewed</h2>
                 <div class="most-anticipated-container space-y-10 mt-8">
